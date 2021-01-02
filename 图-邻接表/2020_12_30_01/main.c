e2f67e0aedb49fb985ae249f4878a17ea52c81b0
dl <dulei_nn@msn.cn>
2021-01-02 22:35:46 +0800

    sync

--- 图-邻接表/2020_12_30_01/main.c
+++ 图-邻接表/2020_12_30_01/main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct ArcNode {
     int vexIndex;
@@ -19,8 +20,33 @@ typedef struct VertexNode {
 } VertexNode;
 
 typedef struct {
-    VertexNode vertex[20];
+    VertexNode vertex[8];
 } ALGraph;
+
+static int visited[8];
+void DFS(ALGraph *graph, int i)
+{
+    visited[i] = 1;
+    printf("%d ", graph->vertex[i].data);
+
+    ArcNode *p_node = graph->vertex[i].firstArc;
+    while (p_node != NULL) {
+        if (visited[p_node->vexIndex] == 0) {
+            DFS(graph, p_node->vexIndex);
+        }
+        p_node = p_node->next;
+    }
+}
+
+void DFS_traverse(ALGraph *graph)
+{
+    int i;
+    for (i = 0; i < 8; i++) {
+        if (visited[i] == 0) {
+            DFS(graph, i);
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
